d7f9e25460f44247b6b3e5683c4cbefa7fbdb995
petrostrak <pit.trak@gmail.com>
2023-09-05 11:05:33 +0300

    add update_story_status for JiraDatabase

--- src/db.rs
+++ src/db.rs
@@ -106,7 +106,16 @@ impl JiraDatabase {
     }
 
     pub fn update_story_status(&self, story_id: u32, status: Status) -> Result<()> {
-        todo!()
+        let mut parsed = self.database.read_db()?;
+
+        parsed
+            .stories
+            .get_mut(&story_id)
+            .ok_or_else(|| anyhow!("could not find story in database!"))?
+            .status = status;
+
+        self.database.write_db(&parsed)?;
+        Ok(())
     }
 }
 
